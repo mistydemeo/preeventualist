0ce3b29ce931c35525057e8aa922dffb15080ab1
Misty De Meo <mistydemeo@gmail.com>
2012-08-13 22:19:28 -0500

    config: Fix database.yml location

--- config/config.rb
+++ config/config.rb
@@ -2,4 +2,4 @@ RACK_ENV ||= ENV['RACK_ENV'] || 'development'
 set :environment, RACK_ENV.to_sym
 
 # Load db config and establish connection
-ActiveRecord::Base.establish_connection YAML.load(File.read(File.join(File.dirname(__FILE__), 'config', 'database.yml'))).with_indifferent_access[RACK_ENV]
+ActiveRecord::Base.establish_connection YAML.load(File.read(File.join(File.dirname(__FILE__), 'database.yml'))).with_indifferent_access[RACK_ENV]
