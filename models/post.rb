f8960dca59dd5634ac663272958b06d84d065327
Misty De Meo <mistydemeo@gmail.com>
2012-08-19 17:48:34 -0500

    Move newspost formatting to a helper
    * Moved newspost formatting to a helper
    * Added a new helper which handles displaying all newsposts,
      separating that logic out from the view itself

--- models/post.rb
+++ models/post.rb
@@ -4,11 +4,4 @@ class Post < ActiveRecord::Base
   validates_presence_of :created_on
 
   belongs_to :poster, :class_name => "Poster", :inverse_of => :posts
-
-  def post; <<-EOS
-/ #{created_on.strftime("%d %B %Y")} /
-#{text}
-                                                   - #{salutation+", " if salutation}#{poster.name}
-    EOS
-  end
 end
